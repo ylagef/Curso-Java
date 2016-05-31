78a4bd30d1cad1e0c6671101810b51ebace56360
Yeray Lage <ylagef@gmail.com>
2016-05-31 23:59:14 +0200

    Práctica 1 OK. Práctica 3 OK.

--- Prácticas/src/Practica3/Practica3.java
+++ Prácticas/src/Practica3/Practica3.java
@@ -1,5 +1,7 @@
 package Practica3;
 
+import java.util.Random;
+
 /**
  * Created by Yeray on 31/05/2016.
  */
@@ -7,29 +9,35 @@ public class Practica3 {
 
     public static void main(String[] args) {
 
-        float[][] matriz = new float[5][10];
+        System.out.print("Ejercicio 1\n\n");
+
+        double[][] matriz = new double[5][10];
 
         //Asigna a cada lugar un número.
         for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; ) {
-                matriz[i][j] = (i + 1) * (j + 2);
+            for (int j = 0; j < matriz[i].length; j++) {
+
+                Random r = new Random();
+                matriz[i][j] = r.nextDouble() * 10;
             }
         }
 
         //Suma y promedio:
-        float suma = 0;
+        double suma = 0;
+        int cantidad = 0;
         for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; ) {
+            for (int j = 0; j < matriz[i].length; j++) {
                 suma += matriz[i][j];
+                cantidad++;
             }
         }
-        float promedio = suma / 50;
+        double promedio = suma / cantidad;
 
-        //Maximo y posicion
-        float max = 0;
-        String posicion;
+        //Maximo y posicion:
+        double max = 0;
+        String posicion = "";
         for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; ) {
+            for (int j = 0; j < matriz[i].length; j++) {
                 if (matriz[i][j] > max) {
                     max = matriz[i][j];
                     posicion = "i=" + i + " j=" + j;
@@ -37,7 +45,65 @@ public class Practica3 {
             }
         }
 
-        System.out.println("La suma es: " + suma + "\nEl promedio es: " + promedio + "\nEl máximo es: " + max + " y su posición es: ");
+        System.out.println("La suma es: " + suma + "\nEl promedio es: " + promedio + "\nEl máximo es: " + max + " y su posición es: " + posicion);
+
+        //Imprimir tabla
+        System.out.println("\n\tLA MATRIZ ES: \n");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.print("\nEjercicio 2\n\n");
+
+        int radiacion[][] = new int[5][];
+        int dia = 1;
+
+
+        for (int i = 0; i < radiacion.length; i++) {
+            radiacion[i] = new int[dia];
+            for (int j = 0; j < dia; j++) {
+                radiacion[i][j] = 20 * i;
+
+            }
+            dia++;
+        }
+
+        //Suma y promedio:
+        double sumaR = 0;
+        int cantidadValores = 0;
+        for (int i = 0; i < radiacion.length; i++) {
+            for (int j = 0; j < radiacion[i].length; j++) {
+                sumaR += radiacion[i][j];
+                cantidadValores++;
+            }
+        }
+        double promedioR = sumaR / cantidadValores;
+
+        //Maximo y posicion:
+        double maxR = 0;
+        String posicionR = "";
+        for (int i = 0; i < radiacion.length; i++) {
+            for (int j = 0; j < radiacion[i].length; j++) {
+                if (radiacion[i][j] > maxR) {
+                    maxR = radiacion[i][j];
+                    posicionR = "i=" + i + " j=" + j;
+                }
+            }
+        }
+
+        System.out.println("La suma es: " + sumaR + "\nEl promedio es: " + promedioR + "\nEl máximo es: " + maxR + " y su posición es: " + posicionR);
+
+        //Imprimir matriz de radiaciones
+        System.out.println("\tLA MATRIZ DE RADIACIONES ES: \n");
+        for (int i = 0; i < radiacion.length; i++) {
+            for (int j = 0; j < radiacion[i].length; j++) {
+                System.out.print(radiacion[i][j] + " ");
+            }
+            System.out.println();
+        }
 
     }
 
